4cc64b5931fb1f076e22700e8a104218afb99de3
ButtrMyAgege <136682328+ButtrMyAgege@users.noreply.github.com>
2025-12-01 23:31:05 +0100

    Update package.js

--- bybit-p2p-monitor/package.js
+++ bybit-p2p-monitor/package.js
@@ -1,1 +1,15 @@
-
+{
+  "name": "bybit-p2p-monitor",
+  "version": "1.0.0",
+  "description": "Automated Bybit P2P rate monitoring with GitHub Actions",
+  "main": "monitor.js",
+  "scripts": {
+    "monitor": "node monitor.js"
+  },
+  "keywords": ["bybit", "p2p", "monitoring", "cryptocurrency", "NGN", "USDT"],
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "node-fetch": "^2.7.0"
+  }
+}
